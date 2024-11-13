c663513fbf3f4aff1748ca26b4e4c3cd0fa07445
crwusiz <crwusiz@gmail.com>
2024-11-13 09:10:48 +0900

    update master

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -14,8 +14,8 @@ AnnotatedCameraWidget::AnnotatedCameraWidget(VisionStreamType type, QWidget *par
   pm = std::make_unique<PubMaster>(std::vector<const char*>{"uiDebug"});
 
   main_layout = new QVBoxLayout(this);
-  main_layout->setMargin(UI_BORDER_SIZE * 4);
-  main_layout->setSpacing(20);
+  main_layout->setMargin(UI_BORDER_SIZE);
+  main_layout->setSpacing(0);
 
   experimental_btn = new ExperimentalButton(this);
   main_layout->addWidget(experimental_btn, 0, Qt::AlignTop | Qt::AlignRight);
